eecebf51394641eb3b37c4cdc1a30d00e55de48e
Bateristico <camilo.saa@gmail.com>
2021-07-30 14:46:28 +0200

    firestore queries

--- src/views/playlists/UserPlaylists.vue
+++ src/views/playlists/UserPlaylists.vue
@@ -5,7 +5,18 @@
 </template>
 
 <script>
-export default {};
+import getUser from '@/composables/getUser';
+import getCollection from '@/composables/getCollection';
+
+export default {
+  setup() {
+    const { user } = getUser();
+    const { documents: playlists } = getCollection('playlists', ['userId', '==', user.value.uid]);
+
+    console.log(playlists);
+    return { playlists };
+  },
+};
 </script>
 
 <style></style>
